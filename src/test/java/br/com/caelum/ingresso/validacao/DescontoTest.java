c43c5c267725a1c297577402d7ecb826d7154593
Gustavo <gustsouzan@gmail.com>
2021-05-15 15:00:49 -0300

    Adicionando @Before no DescontoTest.class

--- src/test/java/br/com/caelum/ingresso/validacao/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/DescontoTest.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import java.time.LocalTime;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.caelum.ingresso.model.DescontoParaBancos;
@@ -17,16 +18,18 @@ import br.com.caelum.ingresso.model.Sessao;
 
 public class DescontoTest {
 	
-	/*
-	 * Criar o @before depois.
-	 */
-
-	@Test
-	public void naoDeveConcederDescontoParaIngressoNormal() {
+	private Sessao sessao; 
+	
+	@Before
+	public void preparaSessoes() {
 		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
 		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
 
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		this.sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+	}
+
+	@Test
+	public void naoDeveConcederDescontoParaIngressoNormal() {
 		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
 
 		BigDecimal precoEsperado = new BigDecimal("32.50");
@@ -36,10 +39,6 @@ public class DescontoTest {
 
 	@Test
 	public void deveConcederDescontoDe30PorcentoParaIngressosDeClientesDeBancos() {
-		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
-		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
-		
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
 		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
 		
 		BigDecimal precoEsperado = new BigDecimal("22.75");
@@ -49,10 +48,6 @@ public class DescontoTest {
 
 	@Test
 	public void deveConcederDescontoDe50PorcentoParaIngressoDeEstudante() {
-		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
-		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
-		
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
 		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
 		
 		BigDecimal precoEsperado = new BigDecimal("16.25");
